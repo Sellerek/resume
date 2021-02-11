61f0b2ccd8fd383e077c4f8890581a25980f32d9
Krzysztof Pietruszka <44585480+Sellerek@users.noreply.github.com>
2021-02-11 14:54:25 +0100

    Update About.js

--- src/Components/About.js
+++ src/Components/About.js
@@ -31,7 +31,7 @@ const About = ({ data }) => {
             </div>
             <div className="columns download">
               <p>
-                <a href={data.resumeDownload} className="button">
+                <a href={data.resumedownload} className="button">
                   <i className="fa fa-download"></i>Download Resume
                   </a>
               </p>
